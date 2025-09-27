586ae1ec2761061d82a0f88f134914ba7cc18b78
Amaan Sheikh <94191143+amaan-123@users.noreply.github.com>
2025-09-27 21:32:03 +0530

    Add logic problem docs & answers for two projects: 009(TicketBookingSystem) & 010(TaskCooldownTime) to Practice.sln

---
+++ fs_mentors/009_TicketBookingSystem/009_TicketBookingSystem.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Enter no. of seats & no. of booking requests: ");
+string? input = Console.ReadLine().Trim();
+int L = int.Parse(input.Split(" ", 2, StringSplitOptions.RemoveEmptyEntries)[0]);
+int Q = int.Parse(input.Split(" ", 2, StringSplitOptions.RemoveEmptyEntries)[1]);
+
+int requestedSeatNum;
+int[] assignedSeatNums = new int[L];
+//create seats array
+for (int i = 0; i < L; i++)
+{
+    assignedSeatNums[i] = 0;
+    //Console.Write($"{assignedSeatNums[i]} ");
+}
+
+string[] outputs = new string[Q];
+for (int i = 0; i < Q; i++)
+{
+    requestedSeatNum = int.Parse(Console.ReadLine());
+
+    if ((requestedSeatNum <= L) && assignedSeatNums[requestedSeatNum - 1] == 0)
+    {
+        assignedSeatNums[requestedSeatNum - 1] = requestedSeatNum;
+        outputs[i] = "ACCEPTED";
+    }
+    else
+    {
+        outputs[i] = "REJECTED";
+    }
+}
+
+foreach (var output in outputs)
+{
+    Console.WriteLine(output);
+}
---
+++ fs_mentors/010_TaskCooldownTime/010_TaskCooldownTime.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter the task string(letters only, ensure no space/other characters in between): ");
+string T = Console.ReadLine().Trim().ToUpper();
+Console.WriteLine("Enter the no. of cooldown units to separate identical tasks(a number only): ");
+int C = int.Parse(Console.ReadLine().Trim());
+
+int cooldownCount = 0;
+int taskCount = 0;
+
+for (int i = 1; i < T.Length; i++)
+{
+    if (T[i - 1] == T[i])
+    {
+        cooldownCount += C;
+        taskCount++;
+    }
+    else
+    {
+        taskCount++;
+    }
+}
+
+Console.WriteLine($"Cooldown count: {cooldownCount}\t Actual Tasks count: {taskCount}\t Total Count:{cooldownCount + taskCount}");
