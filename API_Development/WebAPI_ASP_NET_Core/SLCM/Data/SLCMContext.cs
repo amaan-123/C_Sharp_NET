8e6b2afa5aa2b1d0a1f86c2120f18d964acd07c0
Amaan Sheikh <94191143+amaan-123@users.noreply.github.com>
2025-11-21 11:56:18 +0530

    Learn EF Core: Implement Student management features with Entity Framework Core; add SLCMContext, migrations, and update StudentController for CRUD operations

---
+++ API_Development/WebAPI_ASP_NET_Core/SLCM/Data/SLCMContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SLCM.Models;
+
+namespace SLCM.Data
+{
+    public class SLCMContext : DbContext
+    {
+        public SLCMContext(DbContextOptions<SLCMContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().HasData(
+                new Student { Id = 1, Name = "Aisha", Email = "aisha@example.com", Phone = "1111111111" },
+                new Student { Id = 2, Name = "Ravi", Email = "ravi@example.com", Phone = "2222222222" },
+                new Student { Id = 3, Name = "Meena", Email = "meena@example.com", Phone = "3333333333" }
+                );
+        }
+
+        public DbSet<Student> Students { get; set; }
+        //public DbSet<SchoolRepository> SchoolRepositories { get; set; }//TODO
+    }
+}
