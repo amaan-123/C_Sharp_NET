686e32b85a089e4e5f76e66496aa187eee4ad7d0
Amaan Sheikh <94191143+amaan-123@users.noreply.github.com>
2025-12-22 10:43:07 +0530

    Refactor ProductCatalog Clean project to use EFCore with a new Db connection.

---
+++ DesignPatternsArchitecture/Clean/ProductCatalog.Infrastructure/Data/ProductCatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Infrastructure.Data;
+
+public class ProductCatalogDbContext : DbContext
+{
+    public ProductCatalogDbContext(DbContextOptions<ProductCatalogDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
